95c7d8ea17f4df5eb219653ad1ada59d759bc314
Ray <rayflyworld@gmail.com>
2021-04-12 00:07:07 +0900

    add init vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import store from './store'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
 
 const app = createApp(App)
+app.use(store)
 installElementPlus(app)
 app.mount('#app')
