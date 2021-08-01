67b044a1cc43a90604252e58c0f746fdef0cbc52
Ray <rayflyworld@gmail.com>
2021-08-01 18:22:17 +0900

    deploy

--- src/lang/en.js
+++ src/lang/en.js
@@ -59,6 +59,10 @@ export default {
         rule_3: 'en-学術論文データベースに登載されている学術雑誌に掲載された論文が３本以上※責任著者であるものに限る',
         rule_4: 'en-その他法務大臣が認める研究実績',
       },
+      japanese_level: {
+        n1:'I Either graduated from a foreign university with a major in Japanese-language, or have passed the N1 level of the Japanese-Language Proficiency Test or its equivalent.',
+        n2:'II Have passed the N2 level of the Japanese-Language Proficiency Test or its equivalent',
+      },
       jica: {
         rule: 'Have completed training conducted by JICA as part of the Innovative Asia Project implemented by the Ministry of Foreign Affairs',
       },
